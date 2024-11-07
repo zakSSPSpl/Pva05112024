00ad320f8a8e48c69f777498e8614fb1983f01b3
Antonín Novotný <novotny.an.2022@skola.ssps.cz>
2024-11-07 22:35:50 +0100

    Update TabulkaStruktura.sql

--- TabulkaStruktura.sql
+++ TabulkaStruktura.sql
@@ -3,28 +3,27 @@ DROP TABLE IF EXISTS News;
 DROP TABLE IF EXISTS Tags;
 DROP TABLE IF EXISTS AppliedTags;
 
-CREATE TABLE News(
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	news_name TEXT NOT NULL,
-)
+CREATE TABLE News (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    news_name TEXT NOT NULL
+);
 
-CRAETE TABLE Tags(
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	tag_name TEXT NOT NULL,
-)
+CREATE TABLE Tags (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    tag_name TEXT NOT NULL
+);
 
-CRAETE TABLE AppliedTags(
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	news_id INTEGER NOT NULL,
-	tag_id INTEGER NOT NULL,
-
-	FOREIGN KEY(news_id) REFERENCES News(id),
-	FOREIGN KEY(tag_id) REFERENCES Tags(id),
-)
+CREATE TABLE AppliedTags (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    news_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    
+    FOREIGN KEY (news_id) REFERENCES News (id),
+    FOREIGN KEY (tag_id) REFERENCES Tags (id)
+);
 
 SELECT News.id, News.news_name, Tags.tag_name
-JOIN AppliedTags
-	ON News.id==AppliedTags.news_id
-JOIN Tags
-	ON Appliedtags.tag_id==Tags.Id
-GROUP BY News.id
+FROM News
+JOIN AppliedTags ON News.id = AppliedTags.news_id
+JOIN Tags ON AppliedTags.tag_id = Tags.id
+GROUP BY News.id;
